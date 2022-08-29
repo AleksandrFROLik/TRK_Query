57481daeeed19e9b54e0c860450968ffafd40170
AleksandrFROLik <nextfrolik@gmail.com>
2022-08-29 22:26:02 +0300

    add directories hooks and models. Create hook useDebounce

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchUsersQuery } from '../store/github/github.api';
+import { useDebounce } from '../hooks/debounce';
 
 export const HomePage = () => {
-  const {isLoading, isError, data} = useSearchUsersQuery()
+
+  const [search, setSearch] = useState('')
+  const [dropdown, setDropdown] = useState(false)
+  const debounced = useDebounce(search)
+  const {isLoading, isError, data} = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3, // условия при котором запрос не выполняется
+    refetchOnFocus: true, // обновление вкаладки если долго не использовалась
+  })
+
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && data?.length! > 0)
+  }, [debounced, data])
+
   return (
-    <div>
-      home
+    <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+      {isError && <p className='text-center text-red-600'>Something went wrong... </p>}
+
+      <div className='relative w-[560px]'>
+        <input
+          type="text"
+          className='border py-2 px-4 w-full  h-[42] mb-2'
+          placeholder='Search for Github username...'
+          value={search}
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
+
+        {dropdown &&
+        <ul className='list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white'>
+          {isLoading && <p className='text-center'>Loading...</p>}
+          {data?.map(user => (
+            <li key={user.id}
+                className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+            >{user.login}</li>
+          ))}
+        </ul>}
+      </div>
     </div>
   );
 };
--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,15 +1,23 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { IUser, ServerResponse } from '../../models/models';
 
 export const githubApi = createApi({
   reducerPath: 'github/api',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.github.com/'
   }),
+  refetchOnFocus: true,// обновление вкаладки если долго не использовалась
   endpoints: build => ({
-    searchUsers: build.query({
-      query: () => ({
+    searchUsers: build.query<IUser[], string>({
+      query: (search: string) => ({
         url: `search/users`,
-      })
+        params: {
+          q: search, //параметр с документации
+          per_page: 10 // колличество юзеров (документация)
+        }
+      }),
+      transformResponse: (response: ServerResponse<IUser>) => response.items //модифицируем ответ (из response
+      // достаеи только необходимые даннк
     })
   })
 })
